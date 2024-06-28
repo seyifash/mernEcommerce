65f021ebe9499d5488047c0605e3f90c8805bbfe
seyifash <fasugbeoluwaseyi@gmail.com>
2024-06-28 14:58:13 +0100

    dashborad

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -27,8 +27,6 @@ class APIFeatures {
         // Advance filter for price, ratings etc
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
-        console.log(queryStr)
-
         this.query = this.query.find(JSON.parse(queryStr));
         return this;
     }
